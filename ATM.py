f06183ac47637609a074d69e60abb2b25739e3db
Saloni2915 <salonisoni15dec2002@gmail.com>
2023-02-27 21:48:26 +0530

    atm program

---
+++ ATM.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Welcome to Reserve bank of india")
+pin = int(input("Enter your pin no.: "))
+if pin==1234:
+    print("what do you want to do")
+    print("press 1 to withdraw cash")
+    print("press 2 to for balance inquiry")
+    print("press 3 to Quit")
+    
+    query = int(input("press the no. for your query "))
+    if query==1:
+            print("your withdraw cash is: ")
+    elif query==2:
+            print("your total balance is: ")
+    elif query==3:
+            
+else:
+    print("your entered pin is wrong: ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
